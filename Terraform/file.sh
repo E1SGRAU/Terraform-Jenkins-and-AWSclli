4cd9f326ba904f907738ff2493ddfbdaff978fa9
Alex13 <105718520+E1SGRAU@users.noreply.github.com>
2023-10-25 11:59:44 +0300

    Create file.sh
    Create file.sh

---
+++ Terraform/file.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+sudo apt update -y
+sudo apt install default-jre -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+sudo usermod -aG jenkins ubuntu
+sudo usermod -aG ubuntu jenkins
+
+sudo apt -y install awscli
+sudo apt -y update
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install terraform -y
