18a2c6c4441a2591b067cc8393048ad1a67c7813
Alex13 <105718520+E1SGRAU@users.noreply.github.com>
2023-10-25 11:59:23 +0300

    Create variables.tf
    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_accses_key" {
+  type = string
+  default = "___ ___ ___ ___"
+}
+variable "aws_secret_key" {
+  type = string
+  default = "___ ___ ___ ___"
+}
+variable "aws_instance_type" {
+  type = string
+  default = "t3.micro"
+}
+variable "aws_ami" {
+  type = string
+  default = "___ ___ ___ ___"
+}
+variable "aws_key" {
+  type = string
+  default = "___ ___ ___ ___"
+}
